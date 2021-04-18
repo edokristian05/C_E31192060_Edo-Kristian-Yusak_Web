f8579efc318d8f09b67e607833f5851c3c74d128
edokristian05 <80260445+edokristian05@users.noreply.github.com>
2021-04-19 06:05:02 +0700

    menambahkan folder minggu ke-11

---
+++ Minggu 11/application/controllers/Home.php
@@ -0,0 +1,11 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+class Home extends CI_Controller {
+                function __construct(){
+                parent::__construct();
+                $this->load->helper('url');
+}
+public function index(){
+                $this->load->library('template');
+                $this->template->views('Admin/_Template/Template');
+}}
