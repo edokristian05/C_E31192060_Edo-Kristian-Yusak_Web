0303750b0d002dacdbd1426f85bcdb3a67c00bde
edokristian05 <80260445+edokristian05@users.noreply.github.com>
2021-04-19 06:08:29 +0700

    menambahkan folder minggu ke-12

---
+++ Minggu 12/application/views/Admin/_Template/content.php
@@ -0,0 +1,10 @@
+<div class="container-fluid">
+
+                    <!-- Page Heading -->
+                    <div class="d-sn-flex align-items-center justify-content-between nb-4"></div>
+
+                </div>
+                <?php echo $isi; ?>
+                <!-- /.container-fluid -->
+
+            </div>
